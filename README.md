35e9a85076fce31ca87f82aee9496407b5e4f09c
Senorsen <senorsen.zhang@gmail.com>
2020-03-19 19:21:24 +0800

    Add buttons and multiple functions

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 开机自动启动 kiosk 应用，且关机不会由于系统精简而卡住，适用于小米电视 4 。
 
-先安装本项目 apk 和 de.ozerov.fully ，然后按照网上的方法（如 http://cdn.tvapk.com/qsjrom/xiaomi/xiaomiquguanggao.zip ）干掉默认广告和桌面，即可。
+先安装本项目 apk 和 de.ozerov.fully ，以及另一个 launcher （如 氧气桌面 等），及安装 https://github.com/kingthy/TVRemoteIME 以便远程控制，然后按照网上的方法（如 http://cdn.tvapk.com/qsjrom/xiaomi/xiaomiquguanggao.zip ）干掉默认广告和桌面，重启即可。
--- app/src/main/java/com/example/sentvapplication/MainActivity.kt
+++ app/src/main/java/com/example/sentvapplication/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.sentvapplication
 
+import android.app.ActivityManager
+import android.app.ActivityManager.RunningTaskInfo
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.concurrent.thread
@@ -8,15 +16,81 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "SenTVApplication"
+    private val PACKAGE_KIOSK = "de.ozerov.fully"
+    private val PACKAGE_OXYGEN = "com.yangqi.rom.launcher.free"
+    private val PACKAGE_SETTINGS = "com.xiaomi.mitv.settings"
+
+    @Volatile
+    private var autoOpen = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Will open Kiosk...", Toast.LENGTH_LONG).show()
-        thread {
-            Thread.sleep(3000)
-            val launchIntent =
-                packageManager.getLaunchIntentForPackage("de.ozerov.fully")
-            launchIntent?.let { startActivity(it) }
+
+        findViewById<Button>(R.id.btnSettings).setOnClickListener {
+            try {
+                openSettings()
+            } catch (e: Exception) {
+                openApp(PACKAGE_SETTINGS)
+            }
         }
+        findViewById<Button>(R.id.btnOxygen).setOnClickListener { openApp(PACKAGE_OXYGEN) }
+        findViewById<Button>(R.id.btnKiosk).setOnClickListener { openApp(PACKAGE_KIOSK) }
+
+        if (!isTaskRoot) {
+            val intentAction = intent.action
+            if (intent.hasCategory(Intent.CATEGORY_HOME) && intentAction != null && intentAction == Intent.ACTION_MAIN) {
+                finish()
+                return
+            }
+        }
+
+        if (savedInstanceState == null && !checkAppRunning(PACKAGE_KIOSK)) {
+            Toast.makeText(this, getString(R.string.will_open_kiosk), Toast.LENGTH_SHORT).show()
+            thread {
+                Thread.sleep(5000)
+                if (autoOpen) {
+                    openApp(PACKAGE_KIOSK)
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (!autoOpen) {
+            return
+        }
+        autoOpen = false
+        Toast.makeText(this, getString(R.string.cancelled_auto_open), Toast.LENGTH_SHORT).show()
+        val textView = findViewById<TextView>(R.id.notice)
+        textView.text = ""
+    }
+
+    private fun openApp(packageName: String) {
+        val launchIntent =
+            packageManager.getLaunchIntentForPackage(packageName)
+        launchIntent?.let { startActivity(it) } ?:
+        runOnUiThread {
+            Toast.makeText(this, getString(R.string.no_app, packageName), Toast.LENGTH_LONG).show()
+            val textView = findViewById<TextView>(R.id.notice)
+            textView.text = getString(R.string.no_app, packageName)
+        }
+    }
+
+    private fun checkAppRunning(packageName: String): Boolean {
+        val am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val tasks: List<RunningTaskInfo> = am.getRunningTasks(10)
+        tasks.forEach {
+            Log.i(TAG, "checkAppRunning: ${it.topActivity!!.className}")
+            if (it.topActivity!!.packageName.startsWith(packageName)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun openSettings() {
+        startActivityForResult(Intent(Settings.ACTION_SETTINGS), 0)
     }
 }
